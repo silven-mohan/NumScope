e61d518bce34109b7d4cda131c975e5bc5882cfe
Silven Mohan <silven667@gmail.com>
2025-06-20 22:54:46 +0530

    Embeded NumCheck logic

--- Numscope.c
+++ Numscope.c
@@ -720,6 +720,191 @@ void fibonacci_series()
 	printf("}");
 }
 
+/*** Number Checker ***/
+void NumCheck()
+{
+	int n, no, s=0, f=0, i, j, k;
+	printf("Enter any number:");
+	scanf("%d", &n);
+	system("cls");
+	/** Emirp Numbers **/
+	no=n;
+	while(no!=0)
+	{
+		k=no%10;
+		s=(s*10)+k;
+		no=no/10;
+	}
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+			f++;
+			break;
+		}
+	}
+	for(j=2;j<=s/2;j++)
+	{
+		if(s%j==0)
+		{
+			f++;
+			break;
+		}
+	}
+	if((f==0)&&(n!=s))
+	{
+		printf("\n%d is an Emirp Number.", n);
+	}
+	
+	/** Spy Numbers **/
+	no=n;
+	s=0;
+	f=1;
+	while(no!=0)
+	{
+		k=no%10;
+		s=(s*10)+k;
+		f=f*k;
+		no=no/10;
+	}
+	if(s==f)
+	{
+		printf("\n%d is a Spy Number.", n);
+	}
+	
+	/** Even Numbers **/
+	if(n%2==0)
+	{
+		printf("\n%d is an Even Number.", n);
+	}
+	
+	/** Odd Numbers **/
+	if(n%2!=0)
+	{
+		printf("\n%d is a Odd Number.", n);
+	}
+	
+	/** Prime Numbers **/
+	if(n<2)
+	{
+		printf("\n%d is neither a Prime nor Composite Number.", n);
+	}
+	else if(n>=2)
+	{
+		f=0;
+		for(i=2;i<=n/2;i++)
+		{	
+			if(n%i==0)
+			{	
+				f++;
+				break;
+			}
+		}
+		if(f==0)
+		{
+			printf("\n%d is a Prime Number.", n);
+		}
+		/* Composite Number */
+		else
+		{
+			printf("\n%d is a Composite Number.", n);
+		}
+	}
+	
+	/** Twisted Primes **/
+	no=n;
+	while(no!=0)
+	{
+		k=no%10;
+		s=(s*10)+10;
+		no=no/10;
+	}
+	f=0;
+	for(i=2;i<n/2;i++)
+	{
+		if(n%i==0)
+		{
+			f++;
+			break;
+		}
+	}
+	
+	for(j=2;j<s/2;j++)
+	{
+		if(s%j==0)
+		{
+			f++;
+			break;
+		}
+	}
+	
+	if(f==0)
+	{
+		printf("\n%d is a Twisted Prime Number.", n);
+	}
+	
+	/** Perfect Numbers **/
+	s=0;
+	for(i=1;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+			s=s+i;
+		}
+	}
+	if(n==s)
+	{
+		printf("\n%d is a Perfect Number.", n);
+	}
+	
+	/** Niven Numbers**/
+	no=n;
+	s=0;
+	while(no!=0)
+	{
+		k=no%10;
+		s=s+k;
+		no=no/10;
+	}
+	if(n%s==0)
+	{
+		printf("\n%d is an Niven Number.", n);
+	}
+	
+	/** Strong Number **/
+	no=n;
+	s=0;
+	while(no!=0)
+	{
+		k=no%10;
+		f=1;
+		for(i=k;i>=1;i--)
+		{
+			f=f*i;
+		}
+		s=s+f;
+		no=no/10;
+	}
+	if(n==s)
+	{
+		printf("\n%d is a Strong Number.", n);
+	}
+	
+	/** Palindromes **/
+	no=n;
+	s=0;
+	while(no!=0)
+	{
+		k=no%10;
+		s=(s*10)+k;
+		no=no/10;
+	}
+	if(n==s)
+	{
+		printf("%d is a Palindrome.", n);
+	}	
+}
+
 /**Range Input**/
 void range_input()
 {
@@ -756,7 +941,7 @@ void number_series()
 		printf("\n|E. Perfect Number Series \t|K. Twisted Prime Number Series \t|");
 		printf("\n|F. Armstrong Number Series \t|L. Fibonacci Series \t\t\t|");
 		printf("\n-------------------------------------------------------------------------");
-		printf("\nChoose one of the option below:\n");
+		printf("\nChoose one of the option from above:\n");
 		scanf(" %c", &chose);
 		system("cls");
 		range_input();
@@ -823,7 +1008,9 @@ void main()
 		printf("\n| D. Disarium Number Identifier\t| I. Emirp Number Identifier\t|");
 		printf("\n| E. Niven Number Identifier\t| J. Prime Number Identifier\t|");
 		printf("\n-----------------------------------------------------------------");
-		printf("\n\tEnter K for Number Series.");
+		printf("\n_________________________________________________________________");
+		printf("\n| S. Number Series\t\t| T. Number Property Checker\t|");
+		printf("\n-----------------------------------------------------------------\n");
 		scanf(" %c", &ch);
 		system("cls");
 		switch(ch)
@@ -878,9 +1065,14 @@ void main()
 					prime_number();
 						break;
 				}
-			case 'K':
+			case 'S':
 				{
 					number_series();
+					break;
+				}
+			case 'T':
+				{
+					NumCheck();
 					break;
 				}
 			default:
